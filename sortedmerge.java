c0afb6474615a01748f53b901dfed9e440efd0c1
akap97 <59849316+akap97@users.noreply.github.com>
2020-03-12 01:13:58 +0530

    Create sortedmerge.java
    merge two sorted linked list

---
+++ sortedmerge.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode A, ListNode B) {
+    if(A == null) return B; 
+        if(B == null) return A; 
+          
+        if(A.val < B.val)  
+        { 
+            A.next = mergeTwoLists(A.next, B); 
+            return A; 
+        } 
+        else 
+        { 
+            B.next = mergeTwoLists(A, B.next); 
+            return B; 
+        } 
+            
+    }
+}
