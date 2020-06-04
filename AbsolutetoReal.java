c6b6987a440af1b94df13bbf1b96f1a197ed7ee2
akap97 <59849316+akap97@users.noreply.github.com>
2020-06-04 20:55:24 +0530

    Create AbsolutetoReal.java

---
+++ AbsolutetoReal.java
@@ -0,0 +1,31 @@
+ // Initialize previous and current nodes 
+        Node prev = head; 
+        Node curr = head.next; 
+          
+        // Traverse list 
+        while(curr != null) 
+        { 
+            // If curr is smaller than prev, then 
+                        // it must be moved to head 
+            if(curr.data < prev.data) 
+            { 
+                // Detach curr from linked list 
+                prev.next = curr.next; 
+                  
+                // Move current node to beginning 
+                curr.next = head; 
+                head = curr; 
+                  
+                // Update current 
+                curr = prev; 
+            } 
+              
+            // Nothing to do if current element 
+                        // is at right place 
+            else
+            prev = curr; 
+          
+            // Move current 
+            curr = curr.next; 
+        } 
+        return head; 
